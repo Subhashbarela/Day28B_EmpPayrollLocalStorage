6c82bfd1416984335490e47b6cab736f6e5e84a8
Subhash Barela <barelasubhash16@gmail.com>
2023-04-05 14:26:37 +0530

    save empPayroll data into locol storage

--- NewPayroll.js
+++ NewPayroll.js
@@ -30,40 +30,16 @@ const save =()=>{
         return;
     }
 }
-const createEmployeePayroll =() => {
-   let employeePayrollData = new EmployeePayrollData();
-   try{
-    employeePayrollData.name = getInputValueById('#name');
-   } catch(e){
-    setTextvalue('.text-error',e);
-    throw e;
-   }
-   employeePayrollData.profilePic= getSelectedValue('[name=profilePic]').pop();
-   employeePayrollData.gender= getSelectedValue('[name=gender]').pop();
-   employeePayrollData.department= getSelectedValue('[name= department]');
-   employeePayrollData.salary= getSelectedValue('[name=salary]');
-   employeePayrollData.note= getSelectedValue('[name=note]');
-   let date = getInputValueById('#day')+""+getInputValueById('#month')+getInputValueById('#year');
-   employeePayrollData.date = Date.parse(date);
-   alert(employeePayrollData.toString());
-   return employeePayrollData;
 
-}
-const getSelectedValue=(propertyValue) =>{
-    let allItems = document.querySelector(propertyValue);
-    let setItems =[];
-    allItems.forEach(element => {
-        if(element.checked) setItems.push(element.value);
-        
-    });
-    return setItems;
-}
-const getInputValueById =(id) =>{
-    let value = document.querySelector(id).value;
-    return value;
-}
+function createAndUpdateStorage(employeePayrollData){
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
 
-const getInputElementValue =(id) =>{
-    let value = document.getElementById(id).value;
-    return value;
+    if(employeePayrollList !=undefined){
+        employeePayrollList.push(employeePayrollData);
+    }
+    else{
+        employeePayrollList=[employeePayrollData];
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmpoyeePayrollList",JSON.stringify(employeePayrollList));
 }
